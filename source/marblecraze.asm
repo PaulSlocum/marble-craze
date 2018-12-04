ce03a0f01ca7bc83b906214bb95b5aecb7aa6103
Paul Slocum <paul.slocum@gmail.com>
2018-12-04 04:42:30 -0800

    Moved tools to tools folder, still need to adjust batch files

--- source/marblecraze.asm
+++ source/marblecraze.asm
@@ -3,6 +3,14 @@
 ; 2002-2018
 ;---------------------------------------------------------------------------
 
+;------------------------------------------------------------
+; Marble Craze is a 32K game divided in into eight 4K banks
+; since the Atari can only address 4K.  This file contains
+; the first bank and the following banks are included near the 
+; end of this file.
+;------------------------------------------------------------
+
+
 ;------------------------------
 ; Marble Craze bank 1
 ;------------------------------
